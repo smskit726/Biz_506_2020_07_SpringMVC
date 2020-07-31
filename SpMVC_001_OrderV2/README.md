315251a7dc17c11b82e0388c9bb75a8f2b01f6ee
smskit726 <smskit726@gmail.com>
2020-07-31 16:51:56 +0900

    OrderService_Spring_CRUD

---
+++ SpMVC_001_OrderV2/README.md
@@ -0,0 +1,74 @@
+## JSP의 확장기능
+* 전통적인 JSP는 HTML 코드내에 Java코드를 삽입하여 연산을 수행하거나, Controller로부터 보내진 변수를 표현하는 방법을 사용했다.
+* 이 방법은 HTML 코드에 Java 코드를 삽입하는 과정에서 디자인이 무너지는 현상이 자주 발생을 하고, 그렇지 않아도 복잡한 HTML 코드를 더 복잡하게 만든다.
+* 디자이너와 Java 개발자가 협업을 할 때, 트러블을 일으키는 대표적인 상황이 연출된다.
+
+## EL(Express Lang.) 와 JSTL(JSP Standard Tag Lib.)
+* JSP 2.x 규격이 나오면서 HTML 코드에 Java 코드를 삽입하지 않아도 더 유연하게 view를 만들 수 있는 확장기능이 탄생을 한다.
+* JSP에 include file 등 일부 코드만 최소한으로 사용하면서 더 나은 View를 만드는 방법이다.
+
+### EL(expression : 계산식, Lang.)
+* EL tag : ${변수}, ${1200 * 25}
+* Controller로부터 model에 담겨서 전달된 변수값을 문자열로 변환하여 표시하거나 계산식을 이용하여 결과를 문자열로 변환하여 표시하는 용도
+* JSP 1.x : <%= pageContext.getAttribute("ORDER") %> 
+* EL : ${ORDER}
+
+### JSTL(JSP Standard Tag Lib.)
+* JSP 2.x 규격이 사용된 뒤로, 더이상 JSP 표준코드를 사용하지 말자는 움직임이 나오고 다양한 기법을 동원하여 최소한으로 JSP 표준코드를 사용하는 방법이 적용되었다.
+* 그러나 EL tag 만으로는 JSP표준코드를 100% 대체하기가 어렵다.
+* 그렇다고 EL tag를 마냥 확장할수만도 없다.
+* 그래서 필요할 경우 JSP 코드에 '확장된 기능을 추가하여 사용'할 수 있는 표준이 만들어 진다.
+* JSP의 규격이 더이상 변동이 없어도 다양한 확장을 할 수 있게 된다.
+
+#### JSP 1 코드
+
+	<%
+        for(int i=0; i<10; i++){
+    %>
+    <h3>blabla</h3>
+    <%
+        }
+    %>
+
+#### JSTL
+
+	<c:forEach begin="1" end="10" varStatus="index">
+	<h3>blabla</h3>
+	</c:forEach>
+	
+### Spring project에서 exception이 발생했을 때
+* 원칙 1 : exception 블럭을 제일 아래쪽부터 확인하라
+* EL tag를 사용한 JSP : 보통 객체.필드변수 형식의 EL tag에서 필드변수가 없을 때 발생하는 대표적인 Exception
+
+* project가 시작되면서 발생하는 오류 중
+NullPointerException : @Autowired가 빠졌을 때
+NoSuchBeanDefinitionException : @Service, Annotation이 빠진경우, Dao를 만들지 못하는 경우
+
+### 사용자가 서버에 데이터를 보내기
+1. 고정값을 보내는 방법 : a href="주소?변수=값" 형식으로 링크를 만드는 방법.
+이 방법은 값을 변경하려면 소스코드를 변경해야 하는 문제가 있다.
+2. 사용자가 키보드로 입력한 값을 보내는 방법
+* input tag를 사용하여 입력box를 보여주고
+* 값을 입력하면 서버로 한꺼번에 전송하는 방법
+* input tag 들은 form tag로 감싸서 전송을 하게 된다.
+
+	<form>
+		<input>
+		
+		</input>
+		<button>보내기</button>
+	</form>
+	
+* 이러한 방법을 사용하면 query문자열을 ?변수1=값&변수2=값 과 같이 만들지 않아도 자동으로 query 문자열을 생성하여 서버로 전송을 해준다.
+* form tag는 서버의 Request 주소를 지정해서 보내야 한다. : action을 작성
+* form tag에 method라는 속성을 지정할 수 있는데 기본값이 GET 이다. 
+
+#### form tag의 method
+* GET, POST를 주로 사용하고, 최신기술에 PUT, DELETE, HEAD 등이 있다.
+* GET method 방식 : input box에 입력된 데이터를 query 문자열로 변환하여 서버로 전송
+보내는 데이터가 주소창에 노출이되고, 누군가 조작하기가 쉽다.
+GET method 방식은 query문자열의 최대 길이가 255자이며, 그보다 큰 문자열은 잘리게 된다.
+
+* POST method 방식 : HTTP 프로토콜의 BODY 영역에 데이터를 심어서 서버로 전송을 한다.
+문자열방식으로 전송되지 않기때문에 GET방식에 비해 보안이 좋으며 데이터길이에 제한이 없다.
+일반적으로 form, input을 사용한 입력값 전송에는 POST mothod 방식을 사용한다.
---
+++ SpMVC_001_OrderV2/src/main/java/com/biz/order/controller/OrderController.java
@@ -0,0 +1,151 @@
+package com.biz.order.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.biz.order.model.OrderVO;
+import com.biz.order.service.OrderService;
+
+@Controller
+public class OrderController {
+
+	/*
+	 * OrderService interface를 사용하여 oService를 선언하고
+	 * 이미 생성되어 Container에 보관중인 OrderServiceImplV1 객체를
+	 * oService에 주입하라(Dependency Inject)
+	 * 결국 oService 객체를 통하여 method를 호출할 준비가 된다.
+	 */
+	@Autowired
+	private OrderService oService;
+	
+	/*
+	 * 주문서 전체리스트를 DB로부터 가져와서 보여주는 Controller
+	 * 1. Service에서 전체리스트를 요청
+	 * 2. Service는 Dao에게 전체리스트를 요쳥
+	 */
+	@RequestMapping(value = "/list")
+	public String orderList(Model model) {
+		List<OrderVO> orderList = oService.selectAll();
+		
+		model.addAttribute("ORDERS",orderList);
+		
+		//System.out.println("컨트롤러에서 받은 데이터");
+		//System.out.println(orderList);
+		
+		return "order/list";
+	}
+	
+	// a href="input"로 설정된 링크를 클릭했을 때 응답할 method
+	@RequestMapping(value="/input", method=RequestMethod.GET)
+	public String input() {
+		return "order/input";
+	}
+
+	// input form에서 action으로 지정하여 호출할 method
+	@RequestMapping(value="/input", method=RequestMethod.POST)
+	public String input(@ModelAttribute OrderVO orderVO, Model model) {
+		
+		//System.out.println(orderVO);
+		int ret = oService.insert(orderVO);
+		return "redirect:/list";
+	}
+
+	
+	/*
+	 * browser에서 order request를 실행할 때(요청을 할 때)
+	 * ?변수1=값&변수2=값&변수3=값 형식으로 query를 나열하여 요청을 할 때
+	 * Controller의 method에 변수1, 변수2, 변수3의 매개변수를 마련해두면
+	 * query에 담겨서 전달된 값을 method에서 수신하여 연산에 사용할 수 있다.
+	 */
+	@RequestMapping(value = "/order")
+	public String getOrder(String seq, String name, Model model) {
+		
+		//System.out.println("SEQ 값 : " + seq);
+		//System.out.println("name 값 : " + name);
+		long longSeq = 0;
+		
+		try {
+			longSeq = Long.valueOf(seq);
+		} catch (Exception e){
+			System.out.println("SEQ값을 받지 못함, 잘못된 SEQ를 받음");
+		}
+		
+		// 서비스에 seq(34)를 전달하고
+		// 데이터 레코드를 SELECT 한 결과를 받아서 orderVO에 담는다
+		OrderVO orderVO = oService.findBySeq(longSeq);
+		
+		/*
+		 * view(*.jsp)파일에 전달하여 Rendering을 수행할 수 있도록
+		 * model 객체에 orderVO 데이터를 추가해 놓는다.
+		 */
+		model.addAttribute("ORDER",orderVO);
+		
+		/*
+		 * Dispatcher야 /WEB-INF/views/order/view.jsp 파일을 읽어서
+		 * 클라이언트(요청한 곳) 으로 응답을 해라!
+		 * 이 때, model 에 담겨있는 데이터를 사용하여 Rendering 할 때 사용하라! 
+		 */
+		return "order/detail_view";
+	}
+	
+	@RequestMapping(value="/insert", method=RequestMethod.POST)
+	public String insert(@ModelAttribute OrderVO orderVO, Model model) {
+		
+		//System.out.println(orderVO);
+		int ret = oService.insert(orderVO);
+		return "redirect:/list";
+	}
+	
+	
+	@RequestMapping(value="/insert", method=RequestMethod.GET)
+	public String insert(String o_num, String o_date, String o_cnum,
+						String o_pcode, String o_price,	String o_qty, Model model) {
+		
+		OrderVO orderVO = new OrderVO();
+		orderVO.setO_num(o_num);
+		orderVO.setO_date(o_date);
+		orderVO.setO_cnum(o_cnum);
+		orderVO.setO_pcode(o_pcode);
+		
+		try {
+			orderVO.setO_price(Integer.valueOf(o_price));
+			orderVO.setO_qty(Integer.valueOf(o_qty));
+			orderVO.setO_total(orderVO.getO_price() * orderVO.getO_qty());
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("숫자변환 오류");
+		}
+		
+		int ret = oService.insert(orderVO);
+		
+		/*
+		 * web browser의 주소창에 .../order/list 라고 입력한 후 Enter를 누른 것과 같이
+		 * 명령을 수행하라고 web browser한테 알려주기
+		 */
+		return "redirect:/list";
+		//return null;
+	}
+	
+	@RequestMapping(value="/delete", method=RequestMethod.GET)
+	public String delete(String seq) {
+		
+		long longSeq = 0;
+		try {
+			longSeq = Long.valueOf(seq);
+		} catch (Exception e) {
+			System.out.println("숫자변환 오류");
+		}
+		
+		int ret = oService.delete(longSeq);
+		
+		return "redirect:/list";
+		
+	}
+	
+}
