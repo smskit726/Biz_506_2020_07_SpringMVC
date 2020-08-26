7e18cf7be25ab460082425be61a1fe811e1c9a37
smskit726 <smskit726@gmail.com>
2020-08-26 16:40:45 +0900

    Shop Service (login)

---
+++ SpMVC_ShopV3/src/main/java/com/biz/shop/model/DeptVO.java
@@ -0,0 +1,23 @@
+package com.biz.shop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeptVO {
+	private String d_code;
+	private String d_name;
+	private String d_ceo;
+	private String d_tel;
+	private String d_address;
+	private String d_manager;
+	private String d_mn_tel;
+	private Byte d_not_use;
+}
