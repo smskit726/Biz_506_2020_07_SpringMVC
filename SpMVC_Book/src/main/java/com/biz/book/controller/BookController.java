c64c8d698db67172f5182a82d24cc8d379843e3d
smskit726 <smskit726@gmail.com>
2020-11-24 15:51:16 +0900

    MVC_JPA-BOOK

--- SpMVC_Book/src/main/java/com/biz/book/controller/BookController.java
+++ SpMVC_Book/src/main/java/com/biz/book/controller/BookController.java
@@ -1,12 +1,19 @@
 package com.biz.book.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mapping.AccessOptions.GetOptions.GetNulls;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.biz.book.domain.BookVO;
 import com.biz.book.service.BookService;
 
 @Controller
@@ -17,9 +24,58 @@ public class BookController {
 	private BookService bService;
 	
 	@RequestMapping(value = "/list", method=RequestMethod.GET)
-	public String list(int size, int page) {
+	public String list(@RequestParam(name = "size", required = false, defaultValue = "10") int size,
+						@RequestParam(name="page", required = false, defaultValue = "0") int page, Model model) {
+		
+		page = page == 0 ? 0 : page -1;
 		
 		Pageable pageable = PageRequest.of(page, size);
+		Page<BookVO> pageList = bService.pageSelect(pageable);
+		
+		model.addAttribute("BOOKS", pageList);
+		
 		return "book/list";
 	}
+	
+	@RequestMapping(value = "/insert", method = RequestMethod.GET)
+	public String insert(@ModelAttribute BookVO bookVO, Model model) {
+		
+		model.addAttribute("BOOK", bookVO);
+		return "book/input";
+	}
+	
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public String insert(@ModelAttribute BookVO bookVO) {
+		
+		bService.insert(bookVO);
+		return "redirect:/book/list";
+	}
+
+	@RequestMapping(value = "/view", method = RequestMethod.GET)
+	public String view(String id, Model model) {
+		BookVO bookVO = bService.findById(Long.valueOf(id));
+		model.addAttribute("BOOK",bookVO);
+		return "book/view";
+	}
+	
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	public String update(String id, Model model) {
+		BookVO bookVO = bService.findById(Long.valueOf(id));
+		model.addAttribute("BOOK", bookVO);
+		return "book/input";
+	}
+	
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public String update(BookVO bookVO, Model model) {
+		bService.update(bookVO);
+		model.addAttribute("ID", bookVO.getId());
+		return "redirect:/book/view";
+	}
+	
+	@RequestMapping(value = "/delete", method=RequestMethod.GET)
+	public String delete(String id) {
+		bService.delete(Long.valueOf(id));
+		return "redirect:/book/list";
+	}
+	
 }
---
+++ SpMVC_Book/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tbl_book(id,title,author,comp,price) VALUES(SEQ_BOOK.NEXTVAL, 'DO IT 자바', '홍길동', '이지스퍼블리싱', 21000);
+INSERT INTO tbl_book(id,title,author,comp,price) VALUES(SEQ_BOOK.NEXTVAL, 'DO IT 자바스크립트', '이몽룡', '이지스퍼블리싱', 18000);
+INSERT INTO tbl_book(id,title,author,comp,price) VALUES(SEQ_BOOK.NEXTVAL, 'DO IT 리액트', '성춘향', '이지스퍼블리싱', 16000);
+INSERT INTO tbl_book(id,title,author,comp,price) VALUES(SEQ_BOOK.NEXTVAL, 'DO IT nodejs', '장길산', '이지스퍼블리싱', 24000);
+INSERT INTO tbl_book(id,title,author,comp,price) VALUES(SEQ_BOOK.NEXTVAL, 'DO IT 오라클', '장보고', '이지스퍼블리싱', 20000);
